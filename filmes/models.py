e9d5011ad93ac040fc30d5b0d97f1ee3d18a3d55
Bia <astlebia@gmail.com>
2025-04-10 18:02:46 -0300

    Initial Commit

---
+++ filmes/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Filme(models.Model):
+    NOTA_CHOICES = {
+        0: "0 - Péssimo",
+        1: "1 - Muito Ruim",
+        2: "2 - Ruim",
+        3: "3 - Ok",
+        4: "4 - Bom",
+        5: "5 - Fantástico",
+    }
+    titulo = models.CharField(max_length=50)
+    visto = models.BooleanField(default=False)
+    nota = models.IntegerField(choices = NOTA_CHOICES)
+    review = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.titulo
