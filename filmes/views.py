1ea168cae13097335aa3f734f425fe2f5134060e
Bia <astlebia@gmail.com>
2025-04-11 11:40:46 -0300

    Modificação para proteger key da api

--- filmes/views.py
+++ filmes/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-import requests
+import requests,secure
 
 # Create your views here.
 def listar_filmes_populares(request):
     url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"
     headers = {
         "accept": "application/json",
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNWZjNTg3YzUyODAwZWRiMDMzMjU4OTgxY2I0OWUxZCIsIm5iZiI6MTc0Mzc2NjgwNS4yMTgsInN1YiI6IjY3ZWZjNTE1OGIxZjMyZWI3OWQ5MjI2ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.95uBxYf8ZtbgGH6ZIkPT0sttZBi-cE_zFeM2uMUm8q0"
+        "Authorization": f"Bearer {secure.api_key}"
     }
     response = requests.get(url, headers=headers)
     data = response.json()
